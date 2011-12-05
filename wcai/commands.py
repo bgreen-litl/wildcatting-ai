16fd6c6e3fae9aeeb2bb3f7bc9821a824de69251
healthbystunts <bob@9r33n.net>
2011-12-05 17:10:34 -0500

    Clean up help

--- wcai/commands.py
+++ wcai/commands.py
@@ -12,21 +12,21 @@ log = logging.getLogger("wildcatting-ai")
 class FieldCommand:
     @classmethod
     def add_subparser(cls, parser):
-        subparser = parser.add_parser("field", help="generate oil field data")
+        subparser = parser.add_parser("field", help="Generate oil field data")
         subparser.add_argument("--width", default=80, type=int,
-                               help="field width")
+                               help="oil field width")
         subparser.add_argument("--height", default=24, type=int,
-                               help="field height")
+                               help="oil field height")
         subparser.add_argument("--num", default=1,  type=int,
                                help="number of fields to generate")
         subparser.add_argument("--delim", default=" ", type=str,
                                help="ascii delimiter")
         subparser.add_argument("--inputs", choices=['prob', 'cost', 'tax'],
-            nargs='+', default=['prob', 'cost'],
-            help="specify input fields from ['prob','cost','tax']")
+                               nargs='+', default=['prob', 'cost'],
+                               help="input fields")
         subparser.add_argument("--output", choices=['utility'],
                                default='util',
-                               help="specify output fields from ['util']")
+                               help="output fields")
         subparser.add_argument("--file", type=str, default=None,
                                help="write to specified file")
 
