b92163147bd816ee1df94758c5b3f1ffd7b4eacf
healthbystunts <bob@9r33n.net>
2011-12-08 17:43:39 -0500

    Add nn backed site choosing

--- tests/test_surveying.py
+++ tests/test_surveying.py
@@ -1,4 +1,10 @@
 import unittest
+
+from wildcatting.model import OilField
+from wildcatting.game import (Filler, OilFiller, DrillCostFiller, 
+                              ReservoirFiller, PotentialOilDepthFiller)
+from wildcatting.theme import DefaultTheme
+
 import wcai.surveying
 
 class SurveyingNNTest(unittest.TestCase):
@@ -7,3 +13,23 @@ class SurveyingNNTest(unittest.TestCase):
         nn = wcai.surveying.SurveyingNN()
         print nn
 
+
+class RuntimeSurveyingPolicyTest(unittest.TestCase):
+
+    def test_choose(self):
+        theme = DefaultTheme()
+        field = OilField(80, 24)
+        OilFiller(theme).fill(field)
+        DrillCostFiller(theme).fill(field)
+        PotentialOilDepthFiller(theme).fill(field)
+        ReservoirFiller(theme).fill(field)
+
+        nn = wcai.surveying.SurveyingNN()
+        pol = wcai.surveying.RuntimeSurveyingPolicy(nn)
+
+        coords = pol.choose(field)
+        ## TODO some tests on the results
+
+        
+if __name__ == "__main__":
+    unittest.main()
--- wcai/surveying.py
+++ wcai/surveying.py
@@ -3,6 +3,7 @@ import neurolab as nl
 
 from wildcatting.theme import DefaultTheme
 
+from .data import Region, UtilityEstimator
 
 rnd = random.Random()
 theme = DefaultTheme()
@@ -45,20 +46,42 @@ class QLearningSurveyingPolicy:
 
 
 class RuntimeSurveyingPolicy:
+    val_funcs = [UtilityEstimator(theme, 80 * 24, normalize=True)]
+
     def __init__(self, nn):
         self.nn = nn
 
-    def choose(self, region):
-        """Choose a site to drill in; Return its index."""
-        # uses probalistic wc-zooming
-        outputs = self.nn.activate(region.inputs)
+    @staticmethod
+    def select_output(outputs):
         tot_out = reduce(lambda x, y: x + y, outputs)
         r = random.uniform(0, 1)
         t = 0
         for i, o in enumerate(outputs):
             t += o / tot_out
             if r <= t:
-                if region.scale == 1:
-                    return i
-                else:
-                    return self.choose(region.zoom(i))
+                return i
+
+    def select(self, region):
+        inputs = region.inputs(['prob', 'cost'])
+        outputs = self.nn.activate(inputs)
+        return RuntimeSurveyingPolicy.select_output(outputs)
+
+    # region is some 1:1 subset of the field
+    def _choose(self, region, scale):
+        if scale == 1:
+            i = self.select(region)
+            c = (i % region.width, i / region.height)
+            x = region.center[0] - region.size[0] / 2 + c[0]
+            y = region.center[1] - region.size[1] / 2 + c[1]
+            return x, y
+
+        r = Region.reduce(region, scale)
+        i = self.select(r)
+        map = Region.map(region.field, RuntimeSurveyingPolicy.val_funcs,
+                         r.center, (region.width / 2, region.height / 2))
+        return self._choose(map, scale / 2)
+
+    def choose(self, field):
+        map = Region.map(field, RuntimeSurveyingPolicy.val_funcs)
+        coords = self._choose(map, 8)
+        return coords
