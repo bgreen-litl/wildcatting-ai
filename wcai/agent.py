2bed9da2f80f3189c6cef5dea99478ef36a603b7
healthbystunts <bob@9r33n.net>
2011-12-14 09:57:41 -0500

    Document the current state of affairs

--- wcai/agent.py
+++ wcai/agent.py
@@ -56,6 +56,18 @@ class Agent:
         pass
 
 
+# Components are decision making entities which are currently all backed by
+# neural networks. The Component object provides a common interface for
+# initializing, saving, loading and training these components. While the RL
+# components (Surveying, Report, Drilling, and Sales) are intended to be
+# trained via Q-learning, they all support supervised learning based on
+# training data files. The purpose of this is threefold: testing, proof of
+# concept, and bootstrap.
+#
+# In the bootstrap case, an NN may even be trained on a smaller set of inputs
+# than will be provided at gameplay time. Additional initially zero-weighted
+# inputs may be inserted with the weights for those inputs learned only
+# through RL.
 class Component:
     @classmethod
     def init(cls, agent):
@@ -102,6 +114,7 @@ class Component:
 
 
 class Surveying(Component):
+    """Responsible for selecting a site to survey"""
     name = 'surveying'
     # TODO incorporate drill cost input and expected utility output
     inputs = 30  # prob
@@ -109,39 +122,12 @@ class Surveying(Component):
     val_funcs = [OilProbability(theme, 80 * 24, normalize=True)]
 
     # Select the site to survey from a region of the correct size to apply
-    # directly to the NN. Makes a choice with probabilities proportional to
-    # the expected utility as estimated by the NN.
+    # directly to the NN. Chooses the site which corresponds to the highest
+    # output value of the NN.
     def _choose_nn(self, region):
         inputs = region.inputs(['prob'])
         outputs = self.nn.sim([inputs])[0]
-
-        print region
-        s = np.array([float('%0.3f' % o) for o in outputs])
-        s.shape = (3, 10)
-        print s
-
-        # select the best for now
-        bi = bo = -1
-        for i, o in enumerate(outputs):
-            if o > bo:
-                bo = o
-                bi = i
-        return bi
-
-        tot_out = reduce(lambda x, y: x + y, outputs)
-
-        r = random.uniform(0, 1)
-        t = 0
-        for i, o in enumerate(outputs):
-            t += o / tot_out
-            if r <= t:
-                break
-
-        s = np.array([float('%0.3f' % o) for o in outputs])
-        s.shape = (3, 10)
-        print s
-
-        return i
+        return np.argmax(outputs)
 
     # Choose a site to survey from the specified region of the field. The
     # region here is always at 1:1 but varies in size. The scale is the factor
@@ -161,35 +147,88 @@ class Surveying(Component):
         return self._choose(map, scale / 2)
 
     def choose(self, field):
-        """Choose a site to survey in the specified field"""
+        """Choose a site to survey in the specified field based on nn output"""
         map = Region.map(field, Surveying.val_funcs)
         coords = self._choose(map, 8)
         return coords
 
+    @staticmethod
+    def highest_prob(field):
+        """Choose the site with the highest known probability"""
+        bp = 0
+        w = field.getWidth()
+        h = field.getHeight()
+        # Consider all the sites, but don't favor the upper left
+        start_row = random.randint(0, h)
+        start_col = random.randint(0, w)
+        for row in xrange(start_row, start_row + h):
+            for col in xrange(start_col, start_col + w):
+                site = field.getSite(row % h, col % w)
+                if site.isSurveyed():
+                    p = site.getProbability()
+                    if p > bp:
+                        bc = (col % w, row % h)
+                        bp = p
+        return bc
+
 
 class Report(Component):
+    """Responsible for deciding whether to drill given a Surveyor's Report"""
     name = 'report'
     inputs = 4   # prob, cost, tax
     outputs = 2  # expected utility of drilling and of not drilling
 
 
 class Drilling(Component):
+    """Responsible for deciding whether to drill 10 more meters"""
     name = 'drilling'
     inputs = 3   # cost, depth, expected depth
     outputs = 2  # expected utiltiy of drilling and of not drilling
 
 
 class Sales(Component):
+    """Responsible for deciding whether to sell a given well"""
     name = 'sales'
     inputs = 3   # income, tax, age
     outputs = 2  # expected utility of selling and of not selling
 
 
+# The Probability and DrillCost components are backed by autoassociative
+# neural networks. The task of these NNs is to guess at a complete distribution
+# given the limited known inputs of surveyed sites.
+#
+# Unlike the supervised learning components, these are intended to be trained
+# by supervised learning only with no modifications applied during
+# reinforcement training. These networks need to be trained on thousands of
+# generated, fully known probability or drill cost distributions.
+#
+# Performing this on a full 80x24 grid seems to be prohibitively costly, so
+# the proposed algorithm operates instead on partitions of the full field.
+# Here's how it works:
+#
+# The training data is based on thousands of 10x3 partitions of full 80x24
+# generated fields with the inputs and outputs being the same, normalize 'prob'
+# for the Probability component, and normalized 'cost' for the DrillCost
+# component.
+#
+# Once trained, a full probability distribution can be theorized based on a
+# gameplay field with a subset of sites surveyed, by the following algorithm.
+#
+#  - generate 50% overlapping (in both directions) to cover the whole field
+#  - apply each such region to the backing autoassociative NN
+#  - update each site in the field according to a weighted average from the NN
+#      outputs for each region that overlaps it. (weights are based on number
+#      of surveyed sites in the region)
+#  - profit
 class Probability(Component):
     """Theorize a probability distribution"""
     name = 'probability'
-    inputs = 80 * 24
-    outputs = 80 * 24
+    inputs = 30
+    outputs = 30
+    val_funcs = [OilProbability(theme, 30, normalize=True)]
+
+    def theorize(self, field):
+        pass
 
 
 class DrillCost(Component):
@@ -197,3 +236,6 @@ class DrillCost(Component):
     name = 'drill_cost'
     inputs = 30
     outputs = 30
+
+    def theorize(self, field):
+        pass
